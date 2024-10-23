ce298fb411299475e0c399f02e1edbf59aaa0cbd
Giorgigujabidze <gujabidzegiorgi35@gmail.com>
2024-10-23 18:56:06 +0400

    implemented mean structural similarity index algorhithm

--- frame_analysis.cpp
+++ frame_analysis.cpp
@@ -1,10 +1,10 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/quality/qualityssim.hpp>
 #include "config.hpp"
-#include "metrics.hpp"
 
 
-
-bool isKeyFrame(const cv::Mat &frame, const cv::Mat &prevFrame, double &threshold, const float **histRange, const int &histSize,
+bool isKeyFrame(const cv::Mat &frame, const cv::Mat &prevFrame, double &threshold, const float **histRange,
+                const int &histSize,
                 int &frameCounter, int forcedKeyframeInterval) {
     cv::Mat histOne, histTwo;
 
@@ -47,8 +47,57 @@ bool isKeyFrame(const cv::Mat &frame, const cv::Mat &prevFrame, double &threshol
     return diffMean > threshold;
 }
 
+double getMSSIM(const cv::Mat &frame, const cv::Mat &prevFrame) {
+    const double C1 = 6.5025, C2 = 58.5225;
+    cv::Mat i1, i2;
 
-bool detectArtifacts(const cv::Mat &frame, const cv::Mat &prevFrame, std::vector<double> &buffer, const std::string &filename,
+    frame.convertTo(i1, CV_32F);
+    prevFrame.convertTo(i2, CV_32F);
+
+    cv::Mat i1Squared = i1.mul(i1);
+    cv::Mat i2Squared = i2.mul(i2);
+    cv::Mat i1Xi2 = i1.mul(i2);
+
+    cv::Mat mu1, mu2;
+    cv::GaussianBlur(i1, mu1, cv::Size(11, 11), 1.5);
+    cv::GaussianBlur(i2, mu2, cv::Size(11, 11), 1.5);
+
+    cv::Mat mu1Squared = mu1.mul(mu1);
+    cv::Mat mu2Squared = mu2.mul(mu2);
+    cv::Mat mu1Xmu2 = mu1.mul(mu2);
+
+    cv::Mat sigma1, sigma2, sigma12;
+
+    cv::GaussianBlur(i1Squared, sigma1, cv::Size(11, 11), 1.5);
+    sigma1 -= mu1Squared;
+    cv::GaussianBlur(i2Squared, sigma2, cv::Size(11, 11), 1.5);
+    sigma2 -= mu2Squared;
+    cv::GaussianBlur(i1Xi2, sigma12, cv::Size(11, 11), 1.5);
+    sigma12 -= mu1Xmu2;
+
+    cv::Mat t1, t2, t3;
+
+    t1 = 2 * mu1Xmu2 + C1;
+    t2 = 2 * sigma12 + C2;
+    t3 = t1.mul(t2);
+
+    t1 = mu1Squared + mu2Squared + C1;
+    t2 = sigma1 + sigma2 + C2;
+    t1 = t1.mul(t2);
+
+    cv::Mat ssimMap;
+
+    cv::divide(t3, t1, ssimMap);
+
+    cv::Scalar mssim = cv::mean(ssimMap);
+
+    return mssim[0];
+
+}
+
+
+bool detectArtifacts(const cv::Mat &frame, const cv::Mat &prevFrame, std::vector<double> &buffer,
+                     const std::string &filename,
                      const double &threshold, int max_buffer_size, const float **histRange, const int &histSize) {
     cv::Mat histOne, histTwo;
 
@@ -59,6 +108,7 @@ bool detectArtifacts(const cv::Mat &frame, const cv::Mat &prevFrame, std::vector
     normalize(histTwo, histTwo, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
 
     double correlation = compareHist(histOne, histTwo, cv::HISTCMP_CORREL);
+
 
     if (buffer.size() < max_buffer_size) {
         buffer.push_back(correlation);
@@ -72,6 +122,7 @@ bool detectArtifacts(const cv::Mat &frame, const cv::Mat &prevFrame, std::vector
         std::cout << "Possible frame corruption detected due to low histogram correlation!\n";
     }
     buffer.clear();
+
     // cout << "Frames are similar, no corruption detected.\n";
     return correlationAverage < threshold;
 }
@@ -111,8 +162,9 @@ bool detectBlackFrame(const cv::Mat &frame, const double &threshold) {
     return mean(frame)[0] < threshold;
 }
 
-bool detectStaticFrame(const cv::Mat &frame, const cv::Mat &prevFrame, const double &threshold, std::vector<double> &buffer,
-                       const int maxBufferSize) {
+bool
+detectStaticFrame(const cv::Mat &frame, const cv::Mat &prevFrame, const double &threshold, std::vector<double> &buffer,
+                  const int maxBufferSize) {
     cv::Mat diff;
     absdiff(frame, prevFrame, diff);
     double bufferAverage = 0;
